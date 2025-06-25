c4578b227605aabde0c50daa72d9784f671192c7
Dan Hariton <dhariton@griddynamics.com>
2025-06-25 15:16:43 +0300

    change project structure make it more modular and decoupled

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -1,70 +1,16 @@
-import { useState } from "react";
-import CoreConcept from "./components/CoreConcept/CoreConcept";
+import CoreConcepts from "./components/CoreConcepts";
+import Examples from "./components/Examples";
 import Header from "./components/Header/Header";
-import TabButton from "./components/TabButton/TabButton";
-import { CORE_CONCEPTS } from "./data";
-import { EXAMPLES } from "./data-with-examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((concept) => (
-              <CoreConcept key={concept.title} {...concept} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Example</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {!selectedTopic && <p>Please select a topic.</p>}{" "}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
