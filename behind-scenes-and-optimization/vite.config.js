989a71e9c06af4a52db8f38408c4a780c9fbe3fb
Dan Hariton <dhariton@griddynamics.com>
2025-07-17 09:45:34 +0300

    add behind scenes and optimization

---
+++ behind-scenes-and-optimization/vite.config.js
@@ -0,0 +1,8 @@
+import MillionLint from "@million/lint";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), MillionLint.vite({ auto: true })],
+});
