d4cad4f5bbd27379e013c7e138bba99e0ea29f66
Dan Hariton <dhariton@griddynamics.com>
2025-07-29 09:28:05 +0300

    add auth section

---
+++ authentification/frontend/src/App.js
@@ -0,0 +1,81 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { action as manipulateEventAction } from "./components/EventForm";
+import Authentication, { action as authAction } from "./pages/Authentication";
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
+import EventDetailPage, {
+  action as deleteEventAction,
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import { action as Logout } from "./pages/Logout";
+import NewEventPage from "./pages/NewEvent";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+import RootLayout from "./pages/Root";
+import { checkAuthLoader, tokenLoader } from "./util/auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    loader: tokenLoader,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ":eventId",
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+                loader: checkAuthLoader,
+              },
+            ],
+          },
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+            loader: checkAuthLoader,
+          },
+        ],
+      },
+      { path: "auth", element: <Authentication />, action: authAction },
+      {
+        path: "newsletter",
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+      {
+        path: "logout",
+        action: Logout,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
