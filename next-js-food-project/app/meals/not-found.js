90be50f214c6a00c2b4eaa6cf50087c03db056dc
Dan Hariton <dhariton@griddynamics.com>
2025-07-31 09:06:08 +0300

    add nextjs food project with app router

---
+++ next-js-food-project/app/meals/not-found.js
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>No such meal</p>
+    </main>
+  );
+}
+
+export default NotFound;
---
+++ next-js-food-project/app/meals/page.js
@@ -0,0 +1,43 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import Link from "next/link";
+import { Suspense } from "react";
+import classes from "./page.module.css";
+
+export const metadata = {
+  title: "All Meals",
+  description: "All the meals shared by our community",
+};
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula
+          rhoncus nibh, eget laoreet tellus sollicitudin id.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Loading...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+}
+
+export default MealsPage;
