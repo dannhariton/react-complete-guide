845f8e02a746cc60979ef5b932e7b3d691adb435
Dan Hariton <dhariton@griddynamics.com>
2025-08-01 09:51:50 +0300

    add animations section

---
+++ animations/src/components/Challenges.jsx
@@ -0,0 +1,64 @@
+import { useContext, useState } from "react";
+
+import { AnimatePresence, motion } from "motion/react";
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import ChallengeItem from "./ChallengeItem.jsx";
+import ChallengeTabs from "./ChallengeTabs.jsx";
+
+export default function Challenges() {
+  const { challenges } = useContext(ChallengesContext);
+  const [selectedType, setSelectedType] = useState("active");
+  const [expanded, setExpanded] = useState(null);
+
+  function handleSelectType(newType) {
+    setSelectedType(newType);
+  }
+
+  function handleViewDetails(id) {
+    setExpanded((prevId) => {
+      if (prevId === id) {
+        return null;
+      }
+
+      return id;
+    });
+  }
+
+  const filteredChallenges = {
+    active: challenges.filter((challenge) => challenge.status === "active"),
+    completed: challenges.filter(
+      (challenge) => challenge.status === "completed"
+    ),
+    failed: challenges.filter((challenge) => challenge.status === "failed"),
+  };
+
+  const displayedChallenges = filteredChallenges[selectedType];
+
+  return (
+    <div id="challenges">
+      <ChallengeTabs
+        challenges={filteredChallenges}
+        onSelectType={handleSelectType}
+        selectedType={selectedType}
+      >
+        <AnimatePresence>
+          {displayedChallenges.length > 0 && (
+            <motion.ol className="challenge-items">
+              {displayedChallenges.map((challenge) => (
+                <AnimatePresence>
+                  <ChallengeItem
+                    key={challenge.id}
+                    challenge={challenge}
+                    onViewDetails={() => handleViewDetails(challenge.id)}
+                    isExpanded={expanded === challenge.id}
+                  />
+                </AnimatePresence>
+              ))}
+            </motion.ol>
+          )}
+        </AnimatePresence>
+        {displayedChallenges.length === 0 && <p>No challenges found.</p>}
+      </ChallengeTabs>
+    </div>
+  );
+}
